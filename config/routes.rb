db1e7983a61ea86063f2f6b9b213a1ba877bb395
Natasha Jadavji <natasha@jadavji.ca>
2016-07-18 21:59:16 +0000

    steps 1 to 3 of rotten mangoes

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # get 'movies/index'
+
+  # get 'movies/show'
+
+  # get 'movies/new'
+
+  # get 'movies/edit'
+  resources :movies
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
