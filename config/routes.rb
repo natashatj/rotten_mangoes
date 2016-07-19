fa8f65ee041055525e6b871498f83c1c4dd36374
Natasha Jadavji <natasha@jadavji.ca>
2016-07-19 01:37:20 +0000

    step 13

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'reviews/new'
+
+  get 'reviews/create'
+
   get 'sessions/new'
 
   get 'sessions/create'
@@ -16,7 +20,9 @@ Rails.application.routes.draw do
 
   # get 'movies/edit'
   resources :sessions, only: [:new, :create]
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
